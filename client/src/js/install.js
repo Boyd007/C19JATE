dead6bae120ad25536e745dca4ead0ebf6040f82
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-07-27 22:41:59 -0400

    test

---
+++ client/src/js/install.js
@@ -0,0 +1,29 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+})
+
+
+butInstall.addEventListener('click', async (event) => {
+    console.log('click');
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+    
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
